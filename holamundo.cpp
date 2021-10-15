4a1408b3dd9c719ba14160e0d7f52939b1187108
Alejandra <asarria@unal.edu.com>
2021-10-15 08:58:06 -0500

    Ejemplos de variables y de hola mundo

---
+++ holamundo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hola mundo";
+  
+  int a;
+  float x;
+  double y = 0.0;
+  std::cout << "a = " << a << "\n";
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n";
+  return 0;
+}
+// Edición
+// Compilación
+// Ejecución
+// Guardo en mi repositorio
+
+// variables
+// operadores
+// funciones
+// estructuras de datos
+// clases
