4efbd8a1bc6c3a3498c6154f9d6a261b9c86bdbf
Alejandra <asarria@unal.edu.com>
2021-10-26 11:08:33 -0500

    Euler: P1

---
+++ 22-10-2021-operators/euler_p1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+  int suma = 0;
+  int m = 0;
+  const int MMAX = 1000;
+  
+  for(m = 1; m < MMAX; m = m+1) {
+  if ((m%3 == 0) || (m%5 == 0)) {
+    //suma = suma  + m;
+    suma +=  m;
+    }
+  }
+  
+  std::cout << suma << "\n";
+  
+  return 0;
+}
