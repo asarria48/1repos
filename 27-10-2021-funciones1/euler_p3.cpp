bb81ba291c681e02c7388684b0407d98071f07f0
Alejandra <asarria@unal.edu.com>
2021-10-28 19:24:04 -0500

    Euler: P3

--- 27-10-2021-funciones1/euler_p3.cpp
+++ 27-10-2021-funciones1/euler_p3.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 
-/*
-  N -> long long
-  calcular los divisores primos
-  calcular el divisor
-  verificar si el divisor es primo
-*/
+bool isprime (long long n); //declaración
+long long largestprimedivisor (long long n);
 
 int main(void)
 {
   const long long N = 600851475143;
-  //const long long N = 1000;
-  for(long long int ii = 2; ii < N; ii++) {
-    if (N%ii == 0){
-      // std::cout << ii << std::endl;
-      //verificar si es primo
-      //std::cout << ii << std::endl;
-      long long largedivisor = N/ii;
-      //std::cout << largedivisor << std::endl;
-      bool isprime = true;
-      for(long long jj = 2; jj < largedivisor; jj++) {
-	if (largedivisor%jj == 0) {
+  std::cout << largestprimedivisor(N) << std::endl;
+
+  return 0;
+}
+
+long long largestprimedivisor(long long n)
+{
+  long long result = n;
+  for(long long int ii = 2; ii < n; ii++) {
+    if (n%ii == 0){
+      long long largedivisor = n/ii;
+      if (isprime(largedivisor) == true){
+       result = largedivisor;
+      break;
+      }
+    }
+  }
+	return result;
+}
+bool isprime (long long n)
+{
+bool isprime = true;
+      for(long long jj = 2; jj < n; jj++) {
+	if (n%jj == 0) {
 	  isprime = false;
 	  break;
-	}
-      }
-      
-      if (isprime) {
-	std::cout << largedivisor << std::endl;
-	break;
-       }
-    }
+	  }
  } 
 
-  return 0; 
+  return isprime; 
 }
